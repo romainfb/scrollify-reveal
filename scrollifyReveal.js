235f23985bd83516efb89ca628287eac8c85cc5a
Romain Fouillade-Bardet <104829999+romainfb@users.noreply.github.com>
2023-10-04 15:57:35 +0200

    Update and rename scrollReveal.js to scrollifyReveal.js

--- scrollifyReveal.js
+++ scrollifyReveal.js
@@ -1,73 +1,74 @@
-class ScrollReveal {
-    
+class ScrollifyReveal {
+
     constructor() {
-        // Options par défaut pour les animations de défilement
+        // Default options for scrolling animations
         this.defaultOptions = {
-            delay: 0, // Délai avant le début de l'animation
-            distance: '0px', // Distance de translation verticale
-            duration: '500ms', // Durée de l'animation
-            easing: 'cubic-bezier(0.5, 0, 0, 1)', // Fonction de temporisation (courbe de vitesse)
-            zoom: 0.8,
-            hideOnExit: true, // Activer ou désactiver la disparition lorsqu'ils sortent de la vue
-            opacity: 1,
+            delay: 0, // Deadline before start of animation
+            distance: '0px', // Vertical travel distance
+            duration: '500ms', // Animation duration
+            easing: 'cubic-bezier(0.5, 0, 0, 1)', // Timer function (speed curve)
+            zoom: 0.8, // Zoom on appear
+            hideOnExit: true, // Enable or disable disappearance when out of view
+            opacity: 1, // Final opacity
         };
     }
 
     reveal(selector, options) {
-        // Sélectionne tous les éléments correspondant au sélecteur
+
+        // Selects all items corresponding to the selector
         const elements = document.querySelectorAll(selector);
 
-        // Fusionne les options par défaut avec les options fournies
+        // Merges default options with supplied options
         options = { ...this.defaultOptions, ...options };
 
-        // Créer un observateur d'intersection pour détecter la visibilité des éléments
+        // Creates an intersection observer to detect the visibility of elements
         const observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
                     const target = entry.target;
 
-                    // Vérifie si l'élément n'a pas déjà été révélé
+                    // Checks if the item has not already been revealed
                     if (!target.classList.contains('revealed')) {
                         setTimeout(() => {
-                            // Applique l'animation d'apparition
+                            // Applies appearance animation
                             target.style.opacity = `${options.opacity}`;
                             target.style.transform = 'scale(1)';
                             target.style.transition = `opacity ${options.duration} ${options.easing}, transform ${options.duration} ${options.easing}`;
                             target.style.transitionDelay = `${options.delay}ms, ${options.delay}ms`;
 
-                            // Marque l'élément comme révélé
+                            // Marks element as revealed
                             target.classList.add('revealed');
                         }, options.delay);
                     }
                 } else if (options.hideOnExit) {
                     const target = entry.target;
 
-                    // Vérifie si l'élément a été révélé précédemment
+                    // Checks if the item has been previously revealed
                     if (target.classList.contains('revealed')) {
-                        // Applique l'animation de disparition
+                        // Applies disappearing animation
                         target.style.opacity = '0';
 
-                        // Définit la transformation en fonction de options.zoom
+                        // Defines the transformation according to options.zoom
                         target.style.transform = `translateY(${options.distance}) scale(${options.zoom})`;
                         
                         target.style.transition = `opacity ${options.duration} ${options.easing}, transform ${options.duration} ${options.easing}`;
                         target.style.transitionDelay = '0s, 0s';
 
-                        // Marque l'élément comme non révélé
+                        // Marks element as not revealed
                         target.classList.remove('revealed');
                     }
                 }
             });
         });
 
-        // Initialise les styles et l'observateur pour chaque élément
+        // Initialize styles and observer for each element
         elements.forEach(element => {
             element.style.opacity = '0';
             element.style.transform = `translateY(${options.distance}) scale(${options.zoom})`;
             element.style.transition = `opacity ${options.duration} ${options.easing}, transform ${options.duration} ${options.easing}`;
             element.style.transitionDelay = '0s, 0s';
 
-            // Observe l'élément pour les changements de visibilité
+            // Observe element for visibility changes
             observer.observe(element);
         });
     }
